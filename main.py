111be6f122e65bbda067f566b56069eaa6ecd534
Laksh Gupta <lakshgupta260@gmail.com>
2026-05-21 14:01:40 +0530

    Fixed Azure startup issue

--- main.py
+++ main.py
@@ -2,15 +2,13 @@ from dotenv import load_dotenv
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.azure_openai import AzureOpenAI
 from llama_index.core import Settings
-from llama_index.core import Settings
 from fastapi import FastAPI, UploadFile, File
 from azure.storage.blob import BlobServiceClient
-import pandas as pd
 from azure.cosmos import CosmosClient
-from llama_index.core import VectorStoreIndex, Document
 import os
 
 load_dotenv()
+
 AZURE_OPENAI_KEY = os.getenv("AZURE_OPENAI_KEY")
 AZURE_OPENAI_ENDPOINT = os.getenv("AZURE_OPENAI_ENDPOINT")
 CONNECTION_STRING = os.getenv("CONNECTION_STRING")
@@ -19,15 +17,20 @@ COSMOS_ENDPOINT = os.getenv("COSMOS_ENDPOINT")
 COSMOS_KEY = os.getenv("COSMOS_KEY")
 COSMOS_DATABASE = os.getenv("COSMOS_DATABASE")
 COSMOS_CONTAINER = os.getenv("COSMOS_CONTAINER")
-cosmos_client = CosmosClient(COSMOS_ENDPOINT, COSMOS_KEY)
 
-database = cosmos_client.get_database_client(COSMOS_DATABASE)
-
-container = database.get_container_client(COSMOS_CONTAINER)
-
-Settings.embed_model = HuggingFaceEmbedding(
-    model_name="BAAI/bge-small-en-v1.5"
+cosmos_client = CosmosClient(
+    COSMOS_ENDPOINT,
+    COSMOS_KEY
 )
+
+database = cosmos_client.get_database_client(
+    COSMOS_DATABASE
+)
+
+container = database.get_container_client(
+    COSMOS_CONTAINER
+)
+
 Settings.llm = AzureOpenAI(
     model="gpt-4.1-mini",
     deployment_name="gpt-4.1-mini",
@@ -39,13 +42,11 @@ Settings.llm = AzureOpenAI(
 app = FastAPI()
 
 
-index = None
-
-
 @app.on_event("startup")
 async def startup_event():
 
     print("Application started")
+
 
 @app.get("/")
 def home():
@@ -63,18 +64,27 @@ async def upload_file(file: UploadFile = File(...)):
         CONNECTION_STRING
     )
 
-    container_client = blob_service_client.get_container_client("raw")
+    container_client = blob_service_client.get_container_client(
+        "raw"
+    )
 
-    blob_client = container_client.get_blob_client(file.filename)
+    blob_client = container_client.get_blob_client(
+        file.filename
+    )
 
     file_content = await file.read()
 
-    blob_client.upload_blob(file_content, overwrite=True)
+    blob_client.upload_blob(
+        file_content,
+        overwrite=True
+    )
 
     return {
         "status": "success",
         "filename": file.filename
     }
+
+
 @app.get("/api/v1/files/list")
 def list_files():
 
@@ -82,7 +92,9 @@ def list_files():
         CONNECTION_STRING
     )
 
-    container_client = blob_service_client.get_container_client("raw")
+    container_client = blob_service_client.get_container_client(
+        "raw"
+    )
 
     blobs = container_client.list_blobs()
 
@@ -95,13 +107,18 @@ def list_files():
     return {
         "files": file_list
     }
-    
+
+
 @app.post("/api/v1/agent/chat")
 def chat(question: dict):
 
     user_question = question["question"]
 
-    query_embedding = Settings.embed_model.get_text_embedding(
+    embed_model = HuggingFaceEmbedding(
+        model_name="BAAI/bge-small-en-v1.5"
+    )
+
+    query_embedding = embed_model.get_text_embedding(
         user_question
     )
 
